4b779afc8258ead41006e6ca8391f4627b59583a
minhnguyent546 <minhnguyent546@gmail.com>
2026-06-27 16:32:28 +0700

    chore(scripts): add serving scripts for Qwen3.5-4B

---
+++ scripts/serving/run_vllm_qwen3_5_4b.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+export CUDA_VISIBLE_DEVICES=0
+export SAFETENSORS_FAST_GPU=1
+
+# vLLM runner v2, DFLASH is not supported
+# export VLLM_USE_V2_MODEL_RUNNER=1
+
+# Qwen/Qwen3.5-4B
+# OrionLLM/GRM-2.5
+# unsloth/Qwen3.5-4B-GGUF:UD-Q8_K_XL
+# W4A16: cyankiwi/Qwen3.5-4B-AWQ-BF16-INT4
+# W8A16: cyankiwi/Qwen3.5-4B-AWQ-BF16-INT8
+# FP8: surogate/Qwen3.5-4B-FP8
+PORT=${PORT:-8124}
+MODEL_ID='Qwen/Qwen3.5-4B'
+MAX_NUM_BATCHED_TOKENS=8192
+MAX_MODEL_LEN=49152
+MAX_NUM_SEQS=16
+GPU_MEMORY_UTILIZATION=0.9
+SPEC_DECODING_METHOD=${SPEC_DECODING_METHOD:-DFLASH}
+
+if [[ $SPEC_DECODING_METHOD == "DFLASH" ]]; then
+  SPECULATIVE_CONFIG='{"method": "dflash", "model": "z-lab/Qwen3.5-4B-DFlash", "num_speculative_tokens": 7}'
+elif [[ $SPEC_DECODING_METHOD == "MTP" ]]; then
+  SPECULATIVE_CONFIG='{"method": "mtp", "num_speculative_tokens": 2}'
+else
+  echo "Invalid SPEC_DECODING_METHOD = $SPEC_DECODING_METHOD. Expected one of dflash or mtp" >&2
+  return 1
+fi
+
+echo "Using model: $MODEL_ID"
+echo "Using SPEC_DECODING_METHOD=${SPEC_DECODING_METHOD} with SPECULATIVE_CONFIG=${SPECULATIVE_CONFIG}"
+
+echo "PORT=$PORT MAX_NUM_BATCHED_TOKENS=$MAX_NUM_BATCHED_TOKENS MAX_MODEL_LEN=$MAX_MODEL_LEN GPU_MEMORY_UTILIZATION=$GPU_MEMORY_UTILIZATION MAX_NUM_SEQS=$MAX_NUM_SEQS"
+
+uv run vllm serve "$MODEL_ID" \
+  --port "$PORT" \
+  --trust-remote-code \
+  --async-scheduling \
+  -tp 1 \
+  -dp 1 \
+  --enable-auto-tool-choice \
+  --language-model-only \
+  --distributed-executor-backend mp \
+  --max-num-batched-tokens "$MAX_NUM_BATCHED_TOKENS" \
+  --max-model-len "$MAX_MODEL_LEN" \
+  --max-num-seqs "$MAX_NUM_SEQS" \
+  --enable-prefix-caching \
+  --gpu-memory-utilization "$GPU_MEMORY_UTILIZATION" \
+  --reasoning-parser qwen3 \
+  --tool-call-parser qwen3_coder \
+  --speculative-config "$SPECULATIVE_CONFIG"
