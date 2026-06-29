6cef8248f506cd2ef13ce60ecfa120c3505241c2
minhnguyent546 <minhnguyent546@gmail.com>
2026-06-29 17:31:49 +0700

    feat(scripts): add script for serving LiquidAI/LFM2.5-8B-A1B with vLLM

---
+++ scripts/serving/run_vllm_lfm2_5_8b_a1b.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+export CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES:-0}"
+export SAFETENSORS_FAST_GPU=1
+
+# vLLM runner v2, DFLASH is not supported
+# export VLLM_USE_V2_MODEL_RUNNER=1
+
+PORT=${PORT:-8124}
+MODEL_ID='LiquidAI/LFM2.5-8B-A1B'
+MAX_NUM_BATCHED_TOKENS=8192
+MAX_MODEL_LEN=49152
+MAX_NUM_SEQS=16
+GPU_MEMORY_UTILIZATION=0.9
+TP="${TP:-1}"
+DP="${DP:-1}"
+
+echo "Using model: $MODEL_ID"
+
+echo "PORT=${PORT}, TP=${TP}, DP=${DP}, MAX_NUM_BATCHED_TOKENS=${MAX_NUM_BATCHED_TOKENS}, MAX_MODEL_LEN=${MAX_MODEL_LEN}, GPU_MEMORY_UTILIZATION=${GPU_MEMORY_UTILIZATION}, MAX_NUM_SEQS=${MAX_NUM_SEQS}"
+
+uv run --no-sync vllm serve "$MODEL_ID" \
+  --port "$PORT" \
+  --trust-remote-code \
+  --async-scheduling \
+  -tp "$TP" \
+  -dp "$DP" \
+  --enable-auto-tool-choice \
+  --language-model-only \
+  --distributed-executor-backend mp \
+  --max-num-batched-tokens "$MAX_NUM_BATCHED_TOKENS" \
+  --max-model-len "$MAX_MODEL_LEN" \
+  --max-num-seqs "$MAX_NUM_SEQS" \
+  --enable-prefix-caching \
+  --gpu-memory-utilization "$GPU_MEMORY_UTILIZATION" \
+  --reasoning-parser qwen3 \
+  --tool-call-parser lfm2
