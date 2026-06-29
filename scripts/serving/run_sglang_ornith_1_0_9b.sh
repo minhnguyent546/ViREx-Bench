9aa80205c8f26a76ca741db942e025d392b381ac
minhnguyent546 <minhnguyent546@gmail.com>
2026-06-29 18:07:17 +0700

    feat(scripts): add scripts for serving `deepreinforce-ai/Ornith-1.0-9B`

--- scripts/serving/run_sglang_ornith_1_0_9b.sh
+++ scripts/serving/run_sglang_ornith_1_0_9b.sh
@@ -12,40 +12,16 @@ export SGLANG_ENABLE_DFLASH_SPEC_V2=1
 export SGLANG_ENABLE_OVERLAP_PLAN_STREAM=1
 
 PORT=${PORT:-8124}
-MODEL_ID='Qwen/Qwen3.5-9B'
+MODEL_ID='deepreinforce-ai/Ornith-1.0-9B'
 CHUNKED_PREFILL_SIZE=8192
 CONTEXT_LENGTH=49152
 MEM_FRACTION_STATIC=0.9
-SPEC_DECODING_METHOD=${SPEC_DECODING_METHOD:-MTP}
 TP="${TP:-1}"
 DP="${DP:-1}"
 
 echo "Using model: $MODEL_ID"
-echo "Spec decoding method: $SPEC_DECODING_METHOD"
 
 echo "PORT=${PORT}, TP=${TP}, DP=${DP}, CHUNKED_PREFILL_SIZE=${CHUNKED_PREFILL_SIZE}, CONTEXT_LENGTH=${CONTEXT_LENGTH}, MEM_FRACTION_STATIC=${MEM_FRACTION_STATIC}"
-
-if [ "$SPEC_DECODING_METHOD" = "MTP" ]; then
-  export SGLANG_ENABLE_OVERLAP_PLAN_STREAM=0
-  echo "Overriding SGLANG_ENABLE_OVERLAP_PLAN_STREAM to 0 as Spec MTP do not support this flag yet"
-  SPEC_ARGS=(
-    --speculative-algorithm EAGLE
-    --speculative-num-steps 3
-    --speculative-eagle-topk 1
-    --speculative-num-draft-tokens 4
-  )
-elif [ "$SPEC_DECODING_METHOD" = "DFLASH" ]; then
-  SPEC_ARGS=(
-    --speculative-algorithm DFLASH
-    --speculative-draft-model-path z-lab/Qwen3.5-9B-DFlash
-    --speculative-num-draft-tokens 8
-    --attention-backend fa3
-    --speculative-draft-attention-backend fa4
-  )
-else
-  echo "Unknown SPEC_DECODING_METHOD: $SPEC_DECODING_METHOD" >&2
-  exit 1
-fi
 
 # python -m sglang.compile_deep_gemm
 uv run --no-sync sglang serve \
@@ -64,5 +40,4 @@ uv run --no-sync sglang serve \
   --tool-call-parser qwen3_coder \
   --grammar-backend xgrammar \
   --enable-flashinfer-allreduce-fusion \
-  --mamba-scheduler-strategy extra_buffer \
-  "${SPEC_ARGS[@]}"
+  --mamba-scheduler-strategy extra_buffer
