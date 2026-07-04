13adc7b858a0d10ab7a54f5f386b28dc49c49050
minhnguyent546 <minhnguyent546@gmail.com>
2026-07-05 01:52:43 +0700

    feat(scripts): add serving script for gemma-4-E2B-it with sglang

---
+++ scripts/serving/run_sglang_gemma_4_e2b_it.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+export CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES:-0}"
+export SAFETENSORS_FAST_GPU=1
+
+export SGLANG_ALLOW_OVERWRITE_LONGER_CONTEXT_LEN=1
+export TVM_FFI_GPU_BACKEND=cuda
+
+# Optional: enable schedule overlapping (experimental, may not be stable)
+export SGLANG_ENABLE_SPEC_V2=1
+export SGLANG_ENABLE_DFLASH_SPEC_V2=1
+export SGLANG_ENABLE_OVERLAP_PLAN_STREAM=1
+
+PORT=${PORT:-8124}
+MODEL_ID='google/gemma-4-E2B-it'
+CHUNKED_PREFILL_SIZE=8192
+CONTEXT_LENGTH=32768
+MEM_FRACTION_STATIC=0.875
+SPEC_DECODING_METHOD=${SPEC_DECODING_METHOD:-MTP}
+TP="${TP:-1}"
+DP="${DP:-1}"
+
+echo "Using model: $MODEL_ID"
+echo "Spec decoding method: $SPEC_DECODING_METHOD"
+
+echo "PORT=${PORT}, TP=${TP}, DP=${DP}, CHUNKED_PREFILL_SIZE=${CHUNKED_PREFILL_SIZE}, CONTEXT_LENGTH=${CONTEXT_LENGTH}, MEM_FRACTION_STATIC=${MEM_FRACTION_STATIC}"
+
+if [ "$SPEC_DECODING_METHOD" = "MTP" ]; then
+  export SGLANG_ENABLE_OVERLAP_PLAN_STREAM=0
+  echo "Overriding SGLANG_ENABLE_OVERLAP_PLAN_STREAM to 0 as Spec MTP do not support this flag yet"
+  SPEC_ARGS=(
+    --speculative-algorithm NEXTN
+    --speculative-draft-model-path google/gemma-4-E2B-it-assistant
+    --speculative-num-steps 5 \
+    --speculative-eagle-topk 1 \
+    --speculative-num-draft-tokens 6 \
+  )
+elif [ "$SPEC_DECODING_METHOD" = "OFF" ]; then
+  # No speculative decoding
+  SPEC_ARGS=()
+else
+  echo "Unknown SPEC_DECODING_METHOD: $SPEC_DECODING_METHOD" >&2
+  exit 1
+fi
+
+echo "SPEC_ARGS: ${SPEC_ARGS[*]}"
+
+# python -m sglang.compile_deep_gemm
+uv run --no-sync sglang serve \
+  --model-path "$MODEL_ID" \
+  --port "$PORT" \
+  --tp-size "$TP" \
+  --dp-size "$DP" \
+  --mem-fraction-static "$MEM_FRACTION_STATIC" \
+  --chunked-prefill-size "$CHUNKED_PREFILL_SIZE" \
+  --context-length "$CONTEXT_LENGTH" \
+  --max-running-requests 32 \
+  --cuda-graph-max-bs 32 \
+  --enable-tokenizer-batch-encode \
+  --enable-mixed-chunk \
+  --reasoning-parser gemma4 \
+  --tool-call-parser gemma4 \
+  --grammar-backend xgrammar \
+  --enable-flashinfer-allreduce-fusion \
+  "${SPEC_ARGS[@]}"
