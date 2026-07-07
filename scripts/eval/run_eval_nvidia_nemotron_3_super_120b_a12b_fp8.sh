e17b525aef178f3f2412075e700739ff35b0f83d
minhnguyent546 <minhnguyent546@gmail.com>
2026-07-07 12:52:23 +0700

    feat(scripts): add eval scripts for nemotron-3 and phi-4

---
+++ scripts/eval/run_eval_nvidia_nemotron_3_super_120b_a12b_fp8.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# using DeepSeek API
+export VIREX_BENCH_JUDGE_API_KEY="$DEEPSEEK_API_KEY"
+export VIREX_BENCH_JUDGE_MODEL='deepseek/deepseek-v4-flash'
+
+RESULTS_DIR='./final_results/seed-0'
+
+# STRATEGY="${STRATEGY:-cot}"
+NUM_THREADS="${NUM_THREADS:-32}"
+LOG_LEVEL="${LOG_LEVEL:-INFO}"
+API_BASE="${API_BASE:-https://lm.minhnguyent546.io.vn/v1}"
+NUM_EXAMPLES=''
+
+if [[ -n "$NUM_EXAMPLES" ]]; then
+  EXTRA_PARAMS+=(--max-examples "$NUM_EXAMPLES")
+fi
+
+BASE_EXTRA_PARAMS=("${EXTRA_PARAMS[@]}")
+
+for STRATEGY in direct cot cot-sc3 cot-sc5 tot-beam tot-dfs tot-mcts cr pot_z3; do
+  EXTRA_PARAMS=("${BASE_EXTRA_PARAMS[@]}")
+  DECODING_STRATEGY=single-pass
+  if [[ "$STRATEGY" == "cot-sc3" ]]; then
+    STRATEGY='cot'
+    DECODING_STRATEGY='self-consistency'
+    EXTRA_PARAMS+=(--self-consistency-num-samples 3)
+  elif [[ "$STRATEGY" == "cot-sc5" ]]; then
+    STRATEGY='cot'
+    DECODING_STRATEGY='self-consistency'
+    EXTRA_PARAMS+=(--self-consistency-num-samples 5)
+  fi
+
+  echo ">> Config: STRATEGY=${STRATEGY}, DECODING_STRATEGY=${DECODING_STRATEGY}, NUM_THREADS=${NUM_THREADS}, NUM_EXAMPLES=${NUM_EXAMPLES:-'all'}"
+  echo ">> Extra params: ${EXTRA_PARAMS[*]}"
+
+  uv run --no-sync vb run \
+    --model nvidia/NVIDIA-Nemotron-3-Super-120B-A12B-FP8 \
+    --task vietnamese-logical-reasoning \
+    --strategy "$STRATEGY" \
+    --decoding "$DECODING_STRATEGY" \
+    --backend hosted_vllm \
+    --api-base "$API_BASE" \
+    --api-key '<empty>' \
+    --num-threads "$NUM_THREADS" \
+    --model-kwargs '{"temperature": 1.0, "top_p": 0.95, "chat_template_kwargs": {"enable_thinking": false}}' \
+    --output-dir "$RESULTS_DIR" \
+    --log-level "$LOG_LEVEL" \
+    "${EXTRA_PARAMS[@]}"
+
+done
